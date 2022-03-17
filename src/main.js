86075ff10e35f073bfb835e6e66f2a59e4946c24
Søren Heide Larsen <soren.heide.larsen@norriq.com>
2022-03-17 11:59:27 +0100

    Added rum

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
+import Vue, {createApp} from 'vue'
 import App from './App.vue'
+import { ApmVuePlugin } from '@elastic/apm-rum-vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+let options = {
+    config: {
+    serverUrl: 'https://ucn-rum-apm.norriq.dev:443',
+    serviceName: 'WebApp'
+    }
+};
+Vue.use(ApmVuePlugin, options);
+
+let app = new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
